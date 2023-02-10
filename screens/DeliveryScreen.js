c6880c010d086f05065329e4e35d90bd629a6e9c
Lalafang33 <lalafang33@gmail.com>
2023-02-10 00:09:36 -0800

    added the map compoenent

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -7,11 +7,13 @@ import { selectRestaurant } from "../features/restaurantSlice";
 import { XMarkIcon } from "react-native-heroicons/solid";
 import { urlFor } from "../sanity";
 import * as Progress from "react-native-progress"
+import MapView, { Marker } from 'react-native-maps'
 
 
 export default function DeliveryScreen() {
   const navigation = useNavigation();
-  const restaurant = useSelector(selectRestaurant);
+  const restaurant = useSelector(selectRestaurant).restaurant;
+  // console.log(restaurant.long)
 
   return (
     <View className="bg-[#00CCBB] flex-1">
@@ -38,8 +40,34 @@ export default function DeliveryScreen() {
           </View>
 
           <Progress.Bar  size={30} indeterminate={true} color="#00CCBB" />
+
+          <Text className='mt-3 text-gray-500'>
+            Your order at {restaurant.title}is being prepared
+          </Text>
         </View>
       </SafeAreaView>
+
+      <MapView 
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude:restaurant.long,
+          latitudeDelta:0.005,
+          longitudeDelta:0.005,
+        }}
+        className='flex-1 -mt-10 z-0'
+        mapType="mutedStandard"
+      
+      >
+        <Marker 
+          coordinate={{
+            latitude: restaurant.lat,
+            longitude:restaurant.long,
+          }}
+          title={restaurant.title}
+          identifier='origin'
+          pinColor="#00CCBB"
+        />
+      </MapView>
     </View>
   );
 }
