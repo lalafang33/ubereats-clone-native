012c9e03bb9e9616e2c5542da5b035cdd5e5af8b
Lalafang33 <lalafang33@gmail.com>
2023-02-10 00:15:05 -0800

    completed app

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -68,6 +68,24 @@ export default function DeliveryScreen() {
           pinColor="#00CCBB"
         />
       </MapView>
+
+      <SafeAreaView className='bg-white flex-row items-center space-x-5 h-28'>
+        <Image 
+          source={{
+            uri:"https://links.papareact.com/wru"
+          }}
+          className='h-12 w-12 bg-gray-300 p-4 rounded-full ml-5'
+        />
+        <View className="flex-1">
+          <Text className='text-lg'>
+            Lala Fang
+          </Text>
+          <Text className='text-gray-400'>
+            Your Rider
+          </Text>
+        </View>
+        <Text className='text-[#00CCBB] text-lg mr-5 font-bold'>Call</Text>
+      </SafeAreaView>
     </View>
   );
 }
